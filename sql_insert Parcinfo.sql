7263afb0d6d25a65828639eaaf311339fc6d8471
Clement Douale <clement.douale@etu.utc.fr>
2020-05-27 11:23:35 +0200

    Update sql_insert Parcinfo.sql

---
+++ sql_insert Parcinfo.sql
@@ -0,0 +1,46 @@
+ INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix)
+ VALUES 
+   ;
+
+ INSERT INTO Le_ticket (numero,dtPanne,description)
+ VALUES
+  ;
+
+INSERT INTO L_intervention (numero, prix)
+VALUES
+;
+
+ INSERT INTO Technicien (id, nom, prenom, numero_de_telephone)
+ VALUES
+    ;
+
+ INSERT INTO La_configuration (dtAchat,
+   ;
+
+
+ INSERT INTO Le_fournisseur (code, nom, numero_de_telephone, adresse_e_mail)
+ VALUES
+  ;
+
+
+ INSERT INTO La_localite (id, numero, rue, code_postal, nom_de_ville)
+ VALUES ;
+
+
+ INSERT INTO Le_contrat_de_maintenance (id, mode_de_paiement, montant, dtSignature)
+ VALUES
+    ;
+
+
+ INSERT INTO Unite_de_gestion (nom, nombre_de_membres)
+ VALUES
+    ;
+
+
+ INSERT INTO Societe (nom, adresse, nombre d_employes)
+ VALUES
+    ;
+
+
+ INSERT INTO Affectation (dtAffectation)
+    ;
