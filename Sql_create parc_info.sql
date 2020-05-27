ae9c518b19576535caad7d8a1ee1a9f5ba1bac96
Clement Douale <clement.douale@etu.utc.fr>
2020-05-27 11:08:23 +0200

    Update Sql_create parc_info.sql

--- Sql_create parc_info.sql
+++ Sql_create parc_info.sql
@@ -1,32 +1,37 @@
 CREATE TABLE Equipement (
-    #numero de serie : SERIAL 
+    numero_de_serie  SERIAL ,
     nature  VARCHAR ,
     type  VARCHAR,
-    modèle VARCHAR,
+    modele VARCHAR,
     marque  VARCHAR,
     prix  integer,
+    PRIMARY KEY (numero_de_serie)
     );
 
 CREATE TABLE Le_ticket (
     #numero  SERIAL,
     dtPanne date,
     description  VARCHAR,
+    PRIMARY KEY (numero)
     );
 
 CREATE TABLE L_intervention (
     #numero  SERIAL,
     prix  integer,
+    PRIMARY KEY (numero)
     );
 
 CREATE TABLE Technicien (
     #id  SERIAL,
     nom  VARCHAR,
-    prénom  VARCHAR,
+    prenom  VARCHAR,
     numero_de_telephone  INTEGER,
+    PRIMARY KEY (id)
     );
 
 CREATE TABLE La_configuration (
     dtAchat  date
+    PRIMARY KEY
     );
 
 
@@ -35,6 +40,7 @@ CREATE TABLE Le_fournisseur (
     nom  VARCHAR,
     numero_de_telephone  INTEGER,
     adresse_e_mail  VARCHAR,
+    PRIMARY KEY (code)
     );
 
 
@@ -44,6 +50,7 @@ CREATE TABLE La_localite (
     rue  VARCHAR,
     code_postal  integer,
     nom_de_ville  VARCHAR,
+    PRIMARY KEY (id)
     );
 
 
@@ -52,12 +59,14 @@ CREATE TABLE Le_contrat_de_maintenance (
     mode_de_paiement  VARCHAR CHECK (mode_de_paiement in('especes','carte_bleue','chèque','virement')),
     montant INTEGER,
     dtSignature  date,
+    PRIMARY KEY (id)
     );
 
 
 CREATE TABLE Unite_de_gestion (
     #nom  VARCHAR,
     nombre_de_membres INTEGER,
+    PRIMARY KEY (nom)
     );
 
 
@@ -65,9 +74,11 @@ CREATE TABLE Societe (
     #nom  VARCHAR,
     adresse  VARCHAR,
     nombre d'employes INTEGER
+    PRIMARY KEY (nom)
     );
 
 
 CREATE TABLE Affectation (
     dtAffectation : date
+    PRIMARY KEY
     );
