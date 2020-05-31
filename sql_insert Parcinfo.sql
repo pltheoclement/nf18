967999fa00687adb635eacb773a04fd6a4b33e25
Clement Douale <clement.douale@etu.utc.fr>
2020-05-31 15:24:02 +0200

    Update sql_insert Parcinfo.sql

--- sql_insert Parcinfo.sql
+++ sql_insert Parcinfo.sql
@@ -1,14 +1,21 @@
- INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix)
- VALUES 
-   ;
+ INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix) VALUES (1,'ordianteur','bureautique', 'X751B', 'Asus', 985);
+ INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix) VALUES (745,'imprimante','laser 5p/mn', 'envy7830', 'Hp', 160);
+ INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix) VALUES (412,'imprimante','jet d_encre', 'TS9150', 'Canon', 210);
+ INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix) VALUES (142,'ordianteur','bureautique', 'Macbook PRO', 'Apple', 1500);
+ INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix) VALUES (45,'clavier','mecanique', 'CK550', 'Cooler Master', 70);
+ INSERT INTO Equipement (numero_de_serie, nature, type, modele, marque, prix) VALUES (45,'ecran','LCD', 'S24D330', 'Samsung', 130);
+   
 
- INSERT INTO Le_ticket (numero,dtPanne,description)
- VALUES
-  ;
+ INSERT INTO Le_ticket (numero,dtPanne,description,Unite) VALUES (1,'20-05-2020','ecran Samsung ne démarre plus','Mercure');
+ INSERT INTO Le_ticket (numero,dtPanne,description,Unite) VALUES (2,'1-01-2020','probleme de carte reseau sur ordinateur Asus','Terre');
+ INSERT INTO Le_ticket (numero,dtPanne,description,Unite) VALUES (3,'29-04-2020','imprimante Canon en panne', 'Venus');
+ INSERT INTO Le_ticket (numero,dtPanne,description,Unite) VALUES (4,'04-08-2019','blue screen sur ordinateur Macbook PRO','Mars');
+ 
 
-INSERT INTO L_intervention (numero, prix)
-VALUES
-;
+ INSERT INTO L_intervention (numero, prix,technicien) VALUES (1,30);
+ INSERT INTO L_intervention (numero, prix,technicien) VALUES (1,30);
+ INSERT INTO L_intervention (numero, prix,technicien) VALUES (1,30);
+ INSERT INTO L_intervention (numero, prix,technicien) VALUES (1,30);
 
  INSERT INTO Technicien (id, nom, prenom, numero_de_telephone)
  VALUES
