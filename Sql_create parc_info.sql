5466596f0a3f7e63d89ec4b75ff7cb573fc08b9d
Clement Douale <clement.douale@etu.utc.fr>
2020-05-31 14:25:46 +0200

    Update Sql_create parc_info.sql

--- Sql_create parc_info.sql
+++ Sql_create parc_info.sql
@@ -1,55 +1,62 @@
 CREATE TABLE Equipement (
-    numero_de_serie  SERIAL ,
-    nature  VARCHAR ,
-    type  VARCHAR,
-    modele VARCHAR,
-    marque  VARCHAR,
-    prix  integer,
+    numero_de_serie  SERIAL,
+    nature  VARCHAR NOT NULL,
+    type  VARCHAR NOT NULL,
+    modele VARCHAR NOT NULL,
+    marque  VARCHAR NOT NULL,
+    prix  integer NOT NULL,
     PRIMARY KEY (numero_de_serie)
     );
 
 CREATE TABLE Le_ticket (
     numero  SERIAL,
-    dtPanne date,
-    description  VARCHAR,
+    dtPanne date NOT NULL,
+    description  VARCHAR NOT NULL,
+    FOREIGN KEY Unite REFERENCES Unite_de_gestion(nom),
     PRIMARY KEY (numero)
     );
 
 CREATE TABLE L_intervention (
     numero  SERIAL,
-    prix  integer,
+    prix  integer NOT NULL,
+    FOREIGN KEY technicien REFERENCES Technicien(id),
     PRIMARY KEY (numero)
     );
 
 CREATE TABLE Technicien (
     id  SERIAL,
-    nom  VARCHAR,
-    prenom  VARCHAR,
-    numero_de_telephone  INTEGER,
+    nom  VARCHAR NOT NULL,
+    prenom  VARCHAR NOT NULL,
+    numero_de_telephone INTEGER NOT NULL,
+    FOREIGN KEY fournisseur REFERENCES Fournisseur(code),
     PRIMARY KEY (id)
     );
 
 CREATE TABLE La_configuration (
-    dtAchat  date
-    PRIMARY KEY
+    id SERIAL,
+    dtAchat  date NOT NULL,
+    FOREIGN KEY fournisseur REFERENCES Fournisseur(code),
+    FOREIGN KEY contrat REFERENCES Le_contrat_de_maintenance(id),
+    PRIMARY KEY (id)
     );
 
 
 CREATE TABLE Le_fournisseur (
     code  INTEGER,
-    nom  VARCHAR,
-    numero_de_telephone  INTEGER,
-    adresse_e_mail  VARCHAR,
+    nom  VARCHAR NOT NULL,
+    numero_de_telephone  INTEGER NOT NULL,
+    adresse_e_mail  VARCHAR NOT NULL,
     PRIMARY KEY (code)
     );
 
 
 CREATE TABLE La_localite (
     id SERIAL,
-    numero INTEGER,
-    rue  VARCHAR,
-    code_postal  integer,
-    nom_de_ville  VARCHAR,
+    numero INTEGER NOT NULL,
+    rue  VARCHAR NOT NULL,
+    code_postal  integer NOT NULL,
+    nom_de_ville  VARCHAR NOT NULL,
+    FOREIGN KEY fournisseur REFERENCES Fournisseur(code),
     PRIMARY KEY (id)
     );
 
@@ -57,28 +64,30 @@ CREATE TABLE La_localite (
 CREATE TABLE Le_contrat_de_maintenance (
     id SERIAL,
     mode_de_paiement  VARCHAR CHECK (mode_de_paiement in('especes','carte_bleue','chèque','virement')),
-    montant INTEGER,
-    dtSignature  date,
+    montant INTEGER NOT NULL,
+    dtSignature  date NOT NULL,
+    FOREIGN KEY fournisseur REFERENCES Fournisseur(code),
     PRIMARY KEY (id)
     );
 
 
 CREATE TABLE Unite_de_gestion (
     nom  VARCHAR,
-    nombre_de_membres INTEGER,
-    PRIMARY KEY (nom)
-    );
-
-
-CREATE TABLE Societe ( 
-    nom  VARCHAR,
-    adresse  VARCHAR,
-    nombre d'employes INTEGER
+    nombre_de_membres INTEGER NOT NULL,
     PRIMARY KEY (nom)
     );
 
 
 CREATE TABLE Affectation (
-    dtAffectation : date
-    PRIMARY KEY
+    dtAffectation : date NOT NULL,
+    FOREIGN KEY configuration REFERENCES La_configuration(id),
+    FOREIGN KEY unite REFERENCES Unite_de_gestion(nom),
+    PRIMARY KEY (configuration,unite)
+    );
+    
+CREATE TABLE dateIntervention (
+    dtIntervention date NOT NULL,
+    FOREIGN KEY intervention REFERENCES L_intervention(numero),
+    FOREIGN KEY equipement REFERENCES Equipement(numero_de_serie),
+    PRIMARY KEY (intervention,equipement)
     );
