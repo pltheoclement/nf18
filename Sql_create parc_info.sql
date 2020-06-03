1c322a494fcadb0ce65a354f075c6fb7c78c3932
Clement Douale <clement.douale@etu.utc.fr>
2020-06-03 11:08:25 +0200

    Update Sql_create parc_info.sql

--- Sql_create parc_info.sql
+++ Sql_create parc_info.sql
@@ -50,7 +50,7 @@ CREATE TABLE Le_ticket (
     dtPanne date NOT NULL,
     description  VARCHAR NOT NULL,
     Unite VARCHAR,
-    numero_equip
+    numero_equip INTEGER,
     FOREIGN KEY (numero_equip) REFERENCES Equipement(numero_de_serie)
     FOREIGN KEY (Unite) REFERENCES Unite_de_gestion(nom),
     PRIMARY KEY (numero)
