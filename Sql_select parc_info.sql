539f0c83e524830f436c0bc4f8ca7b0c5c522050
Clement Douale <clement.douale@etu.utc.fr>
2020-06-08 15:51:37 +0200

    Update Sql_select parc_info.sql

--- Sql_select parc_info.sql
+++ Sql_select parc_info.sql
@@ -1,5 +1,20 @@
+
+
+
+SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration = La_configuration.id
+
+
 SELECT * FROM La_configuration INNER JOIN Equipement ON (Equipement.configuration=La_configuration.id)
 
 
 
-SELECT * FROM Equipement INNER JOIN Le_ticket ON (Equipement.numero_de_serie=Le_ticket.numero_equip)
+
+
+
+SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=1
+SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=2
+SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=3 
+SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=4
+
+SELECT * FROM L_intervention INNER JOIN Equipement ON (Equipement.numero_de_serie=L_intervention.numero_equip)
+
