f91475875f9618ad759bc5b6cda75430b8f5bba4
Clement Douale <clement.douale@etu.utc.fr>
2020-06-08 16:43:32 +0200

    Update Sql_select parc_info.sql

--- Sql_select parc_info.sql
+++ Sql_select parc_info.sql
@@ -1,19 +1,20 @@
 
+SELECT * FROM La_configuration INNER JOIN Equipement ON (Equipement.configuration=La_configuration.id) ORDER BY configuration
 
-SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration = La_configuration.id
-
-
-SELECT * FROM La_configuration INNER JOIN Equipement ON (Equipement.configuration=La_configuration.id)
-
-
-SELECT 
+CREATE VIEW Garantie (id,date_garantie_restant) AS
+SELECT La_configuration.id as id , ((dtAchat + integer '1095')- CURRENT_DATE) as date_garantie_restant
+FROM La_configuration
 
 
+CREATE VIEW Prix_config AS 
+SELECT SUM(prix_e) as prix_config 
+FROM Equipement
+WHERE configuration=1
 
-SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=1
 SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=2
 SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=3 
 SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=4
 
 SELECT * FROM L_intervention INNER JOIN Equipement ON (Equipement.numero_de_serie=L_intervention.numero_equip)
 
+SELECT * FROM Le_fournisseur INNER JOIN Technicien ON Le_fournisseur.code =Technicien.fournisseur
