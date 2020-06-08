ed2e6bee3b47f188b4ac2a0880f13de020c721c6
Clement Douale <clement.douale@etu.utc.fr>
2020-06-08 16:00:23 +0200

    Update Sql_select parc_info.sql

--- Sql_select parc_info.sql
+++ Sql_select parc_info.sql
@@ -1,20 +1,19 @@
 
 
-
 SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration = La_configuration.id
 
 
 SELECT * FROM La_configuration INNER JOIN Equipement ON (Equipement.configuration=La_configuration.id)
 
 
+SELECT 
 
 
 
-
-SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=1
-SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=2
-SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=3 
-SELECT SUM(prix) as prix_config FROM Equipement WHERE configuration=4
+SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=1
+SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=2
+SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=3 
+SELECT SUM(prix_e) as prix_config FROM Equipement WHERE configuration=4
 
 SELECT * FROM L_intervention INNER JOIN Equipement ON (Equipement.numero_de_serie=L_intervention.numero_equip)
 
