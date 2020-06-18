9b519ee43945c25892a6bed1698cf68ad988bf85
Clement Douale <clement.douale@etu.utc.fr>
2020-06-18 14:31:13 +0200

    Update Sql_select parc_info.sql

--- Sql_select parc_info.sql
+++ Sql_select parc_info.sql
@@ -1,9 +1,16 @@
+Les configurations en fonction des équipements :
+
 SELECT * FROM La_configuration INNER JOIN Equipement ON (Equipement.configuration=La_configuration.id) ORDER BY configuration
+
+Vue pour savoir le temps restant en garantie d'une configuration :
 
 CREATE VIEW Garantie (id,date_garantie_restant) AS
 SELECT La_configuration.id as id , ((dtAchat + integer '1095')- CURRENT_DATE) as date_garantie_restant
 FROM La_configuration
 ORDER BY date_garantie_restant
+
+
+Création de vue pour savoir le prix d'une configuration :
 
 CREATE VIEW Prix_config AS 
 SELECT SUM(prix_e) as prix_config 
@@ -40,10 +47,11 @@ SELECT SUM(prix_e) as prix_config
 FROM Equipement
 WHERE configuration=7
 
+Sélection de toutes les interventions sur tout les équipements:
+
 SELECT * FROM L_intervention INNER JOIN Equipement ON (Equipement.numero_de_serie=L_intervention.numero_equip)
 
-SELECT numero_equip,type,dtpanne,description FROM L_intervention INNER JOIN Equipement ON (Equipement.numero_de_serie=L_intervention.numero_equip)
-SELECT type,marque,modele,dtpanne,description FROM L_intervention INNER JOIN Equipement ON (Equipement.numero_de_serie=L_intervention.numero_equip)
+Relation entre les pannes et les équipements avec un compteur pour connaître les pannes lesp plus courantes et les autres moins courantes :
 
 CREATE VIEW Panne_type AS
 SELECT equipement.nature,equipement.type, description AS Panne, COUNT (description) as nombredePanne
@@ -51,9 +59,11 @@ FROM L_intervention JOIN equipement ON L_intervention.numero_equip = equipement.
 GROUP BY equipement.nature, equipement.type,L_intervention.description
 ORDER BY COUNT(description)
 
-
+Sélection pour savoir quels techniciens travaillent pour le fournisseur LDLC PRO :
 
 SELECT * FROM Le_fournisseur INNER JOIN Technicien ON Le_fournisseur.code =Technicien.fournisseur WHERE Le_fournisseur.nom_f ='LDLC PRO'
+
+Relations entre les différentes configurations qui ont des contrats de maintenances :
 
 SELECT La_configuration.id,dtAchat,La_configuration.fournisseur,contrat,mode_de_paiement,montant,dtsignature FROM Le_contrat_de_maintenance INNER JOIN La_configuration ON (La_configuration.contrat=Le_contrat_de_maintenance.id)
 
